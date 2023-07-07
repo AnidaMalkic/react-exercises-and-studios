afaefe656b6c7ace78729cbf93ebefe9e7c17d70
AnidaMalkic <malkic.anida@gmail.com>
2023-07-06 23:31:39 -0500

    second commit

---
+++ part1/exercises/src/components/HobbyLinks.js
@@ -0,0 +1,17 @@
+export default function hobbyLinks () {
+    let hobbyLinks = ["https://www.completeguidetoarchery.com/archery-for-beginners-how-to-get-started/", "https://hobbycents.com/hobby-guides/cooking-hobby-guide/"];
+
+    return (
+        <div> 
+            
+            <a href = {hobbyLinks[0]}>Archery As a Hobby</a>
+
+            <br></br>
+
+            <a href = {hobbyLinks[1]}>Cooking As a Hobby</a>
+        </div>
+
+
+
+    );
+}
